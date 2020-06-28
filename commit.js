43b0650808a448de4de61426cc2ea2924ef4fe23
alltime1 <2956489346@qq.com>
2020-06-28 18:02:17 +0800

    first

---
+++ commit.js
@@ -0,0 +1,12 @@
+Function.prototype.call2=function(context){
+  context.fn=this
+  context.fn()
+  delete context.fn
+}
+let obj={
+  age:12
+}
+function getAge(){
+  console.log(this.age)
+}
+getAge.call(obj)
